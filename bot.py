520488679bf6fc9570ab661ec4cb2295eb41be26
amartinez2 <austinamartinez2@gmail.com>
2020-07-05 22:03:27 -0700

    full rotation towards discord bot

--- bot.py
+++ bot.py
@@ -1,41 +1,42 @@
 import discord
 from discord.ext import commands
+import asyncio
 from asyncio import sleep
+import json
+import ledgerControl
+
 
 CONFIG = json.load(open('.config'))
 
 client  = commands.Bot(command_prefix = '.')
 
-# @loop(seconds=10)
-# async def name_change():
-#     print('waffles')
-#     # await client.change_presence(...)
-#     await sleep(10)
-#     # await client.change_presence(...)
 
-# async def my_background_task():
-#     await client.wait_until_ready()
-#     counter = 0
-#     channel = discord.Object(id='channel_id_here')
-#     while not client.is_closed:
-#         counter += 1
-#         await client.send_message(channel, counter)
-#         await asyncio.sleep(5) # task runs every 60 seconds
+async def ledgerLoop():
+    await client.wait_until_ready()
+    channel = client.get_channel(CONFIG['discordChannelId'])
+    while not client.is_closed():
+        ledgerControl.ledgerProcess()
+        await asyncio.sleep(5)
 
 
 @client.event
 async def on_ready():
     channel = client.get_channel(CONFIG['discordChannelId'])
-    await channel.send('@everyone hello')
-    print('wowo')
-    # print(os.system('python main.py'))
-    print('yowza')
+    await channel.send('bot startup')
+    if (not ledgerControl.ledgerFileExists()):
+        ledgerControl.initLedger()
+    await channel.send('Verifying ledger...')
+    if (not ledgerControl.verifyLedger()):
+        await channel.send('Ledger verification failed, initializing new ledger...')
+        ledgerControl.initLedger()
+    else:
+        await channel.send('Ledger verification complete.')
 
 
 async def on_message():
-    print('what')
     channel = client.get_channel(CONFIG['discordChannelId'])
     await channel.send('@everyone hello')
 
 
+client.loop.create_task(ledgerLoop())
 client.run(CONFIG['discordToken'])
